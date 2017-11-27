241a072a2cc8169806855a0cc891b0b6b670c1a0
gehhack <jean-christophe.Gehmert@gehmert.fr>
2017-11-27 19:13:04 +0100

    Update webmin.sh

--- webmin.sh
+++ webmin.sh
@@ -1,6 +1,6 @@
 #!/bin/ash
 
-apk updatae
+apk update
 apk add pwgen
 apk add sudo
 apk add tar
@@ -8,41 +8,43 @@ apk add tar
 WEBMIN_USERNAME=`pwgen -A 8 1`
 WEBMIN_PASSWORD=`pwgen -s 32 1`
 
-WEBMIN_PORT=63245
+WEBMIN_PORT=10238
 WEBMIN_USERNAME=admin
 WEBMIN_PASSWORD=webmin
 
 set -e
 
-sudo apk add curl perl
+apk add curl perl
 #wget https://prdownloads.sourceforge.net/webadmin/webmin-1.860.tar.gz
 
 tar zxf webmin-1.860-minimal.tar.gz
 tar zxf webmin-1.860-minimal.tar.gz -C /var/lib/
-mv /var/lib/webmin-1.860-minimal /var/lib/webmin
-rm -rf webmin-1.860-minimal.tar.gz
+mv /var/lib/webmin-1.860 /var/lib/webmin
+#rm -rf webmin-1.860-minimal.tar.gz
 cd /var/lib/webmin
 mkdir -p /etc/rc.d/init.d/
-cat <<EOF | sudo ./setup.sh
+cat <<EOF | ./setup.sh
 /etc/webmin
 /var/log/webmin
 /usr/bin/perl
+101
+4
 ${WEBMIN_PORT}
 ${WEBMIN_USERNAME}
 ${WEBMIN_PASSWORD}
 ${WEBMIN_PASSWORD}
 y
 EOF
-cat <<EOF | sudo tee /etc/init.d/webmin
+cat <<EOF | tee /etc/init.d/webmin
 #!/sbin/openrc-run
 WEBMIN=/etc/rc.d/init.d/webmin
 start() { \${WEBMIN} start; }
 stop() { \${WEBMIN} start; }
 EOF
-sudo chmod a+x /etc/init.d/webmin
+chmod a+x /etc/init.d/webmin
 
-sudo rc-update add webmin
-sudo rc-service webmin start
+rc-update add webmin
+rc-service webmin start
 
 echo "Informations in /root/access.txt"
 echo "User" >> "/root/access.txt"
