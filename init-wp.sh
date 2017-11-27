a1855b97d6820478d6083cb484c7fadb3b297f06
gehhack <jean-christophe.Gehmert@gehmert.fr>
2017-11-27 12:54:56 +0100

    init-wp.sh
    Install SSH, MariaDB, WP for Alpine 3.5 Proxmox

---
+++ init-wp.sh
@@ -0,0 +1,65 @@
+#!/bin/ash
+
+#Configure SSHD
+setup-sshd
+rc-update add sshd
+/etc/init.d/sshd start
+
+#Repositories
+wget -O /etc/apk/keys/php-alpine.rsa.pub http://php.codecasts.rocks/php-alpine.rsa.pub
+echo "@php http://php.codecasts.rocks/v3.6/php-7.1" >> /etc/apk/repositories
+
+#Install packages
+apk add --update php7-redis@php
+apk add pwgen
+apk add apache2
+Apk add php7
+apk add php7 php7-common php7-fpm php7-cgi php7-apache2 php7-curl php7-gd php7-mbstring php7-mcrypt php7-pdo php7-mcrypt php7-mysqli php7-mysql
+apk add mysql mysql-client
+apk add git
+
+#Git Wordpress
+git clone https://github.com/WordPress/WordPress.git /var/www/localhost/htdocs/
+
+#Apache2 start & boot
+rc-update add apache2
+/etc/init.d/apache2 start
+rm -f /var/www/localhost/htdocs/index.html
+
+#Database
+rc-update add mariadb
+/etc/init.d/mariadb start
+
+USER=`pwgen -A 8 1`
+PASS=`pwgen -s 32 1`
+ROOTPASS=`pwgen -s 32 1`
+DIR='/var/www/localhost/htdocs'
+
+'/usr/bin/mysqladmin' -u root password $ROOTPASS
+'/usr/bin/mysqladmin' -u root -h $HOSTNAME password $ROOTPASS
+
+mysql -uroot -p${ROOTPASS} <<MYSQL_SCRIPT
+CREATE DATABASE $USER;
+CREATE USER '$USER'@'localhost' IDENTIFIED BY '$PASS';
+GRANT ALL PRIVILEGES ON $USER.* TO '$USER'@'localhost';
+FLUSH PRIVILEGES;
+MYSQL_SCRIPT
+
+#Configuration Wordpress
+#Rename the default config file
+cp -f $DIR/wp-config-sample.php $DIR/wp-config.php
+
+#Substitute the default database values
+sed -i "/DB_NAME/s/'[^']*'/'${USER}'/2" $DIR/wp-config.php
+sed -i "/DB_USER/s/'[^']*'/'${USER}'/2" $DIR/wp-config.php
+sed -i "/DB_PASSWORD/s/'[^']*'/'${PASS}'/2" $DIR/wp-config.php
+
+#Statut des services au demarrage
+rc-status
+
+#Infos MDP et USER
+
+echo "User"
+echo "rootPWD :   $ROOTPASS"
+echo "User DB :   $USER"
+echo "Password:   $PASS"
