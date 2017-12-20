81a1745561117a7a5333a7f906280513b219f5b6
gehhack <jean-christophe.Gehmert@gehmert.fr>
2017-12-20 08:38:08 +0100

    init-ssh.sh

---
+++ init-ssh.sh
@@ -0,0 +1,19 @@
+#!/bin/ash
+
+#Clean and update apk
+rm -rf /var/cache/apk/* && rm -rf /tmp/*
+apk update
+
+#Copy motd
+cp -f ./motd /etc/motd
+
+#Configure SSHD
+setup-sshd
+rc-update add sshd
+/etc/init.d/sshd start
+echo "PermitRootLogin Yes" >> "/etc/ssh/sshd_config"
+
+#Statut des services au demarrage
+rc-status
+
+exit
